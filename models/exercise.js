39842919e64ab24d1942330171e1623f1dde108a
Kirk Hallam <kirkh43064@gmail.com>
2021-12-02 16:25:05 -0500

    model

---
+++ models/exercise.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now()
+    },
+    exercise: [
+        {
+            name: {
+                type: String,
+                trim: true,
+                required: 'Exercise NAME is required'
+            },
+            type: {
+                type: String,
+                trim: true,
+                required: 'Exercise TYPE is required'
+            },
+            distance: {
+                type: Number,
+            },
+            duration: {
+                type: Number,
+                required: 'Exercise DURATION is required'
+            },
+            weight: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            }
+        }
+    ]
+});
+
+const Workout = mongoose.model('workout', WorkoutSchema);
+
+module.exports = Workout;
